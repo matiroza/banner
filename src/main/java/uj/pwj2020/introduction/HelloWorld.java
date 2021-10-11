b19118ebaaac97934c41d27cf13de1eb4228ff17
mateusz_rozanowski <m.rozanowski@student.uj.edu.pl>
2021-10-11 14:41:00 +0200

    first commit

--- src/main/java/uj/pwj2020/introduction/HelloWorld.java
+++ src/main/java/uj/pwj2020/introduction/HelloWorld.java
@@ -2,5 +2,8 @@ package uj.pwj2020.introduction;
 
 public class HelloWorld {
     public static void main(String[] args) {
+        for(String arg : args) {
+            System.out.println(arg);
+        }
     }
 }
