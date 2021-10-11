b37435cb9b78609a865601dd91770336399dd2a5
mateusz_rozanowski <m.rozanowski@student.uj.edu.pl>
2021-10-11 14:56:18 +0200

    QuadraticEquation

--- src/main/java/uj/pwj2020/introduction/HelloWorld.java
+++ src/main/java/uj/pwj2020/introduction/HelloWorld.java
@@ -2,6 +2,9 @@ package uj.pwj2020.introduction;
 
 public class HelloWorld {
     public static void main(String[] args) {
+        if (args.length == 0){
+            System.out.println("No input parameters provided");
+        }
         for(String arg : args) {
             System.out.println(arg);
         }
--- src/main/java/uj/pwj2020/introduction/QuadraticEquation.java
+++ src/main/java/uj/pwj2020/introduction/QuadraticEquation.java
@@ -1,9 +1,19 @@
-package uj.pwj2020.introduction;
+/*package uj.pwj2020.introduction;
+
+import java.lang.reflect.Array;
 
 public class QuadraticEquation {
 
     public double[] findRoots(double a, double b, double c) {
+        double delta = b*b-4*a*c;
+        if (delta<0) {
+            return new double[] {};
+        } else if (delta == 0) {
+            return new double[]{ -2*b/a };
+        } else{
+            return new double[]{ -b - Math.sqrt(delta) / 2*a , -b + Math.sqrt(delta) / 2*a };
+        }
     }
 
-}
+}*/
 
